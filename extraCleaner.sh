1df5b80ea458dfdf0d300b05f6aad3128e67c2e6
Mboukhal <85312140+Mboukhal@users.noreply.github.com>
2023-03-04 16:03:38 +0100

    Update extraCleaner.sh

--- extraCleaner.sh
+++ extraCleaner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # remove all files above 60M
+
 cd ~/
+
 input="/tmp/tmp.clean"
 
 ls -lhaR ~/ 2>&1 | grep "M \|G " | grep  '[6-9][0-9]M \|[0-9]G \|[0-9][0-5][0-9]M ' > $input 2>&1
@@ -9,14 +11,12 @@ ls -lhaR ~/ 2>&1 | grep "M \|G " | grep  '[6-9][0-9]M \|[0-9]G \|[0-9][0-5][0-9]
 while read -r line
 do
         x=`echo $line | awk {'print($9)'}`
-        echo $x
         r=`find ~/ -name $x 2>/dev/null`
-        echo $r
         rm "$r" 2> /dev/null
-        echo -e "\033[31m$r Removed.\033[0m"
+        printf "\033[31m$r\tRemoved.\033[0m\n"
 done < "$input"
 
-echo -e "\033[32mExtra clean done\033[0m"
+printf "\033[32mExtra clean done\033[0m\n"
 rm $input
 
 cd -
